d1ea24bff43a179c9067e7a5f9f1d870bd8a0752
Max Goldman <maxg@meet.mit.edu>
2012-12-04 20:39:09 -0500

    Resize non-fallback labels and ignore wide labels

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -14,13 +14,19 @@ document.observe('dom:loaded', function() {
     return 'placeholder' in i;
   }
   if ( ! supports_input_placeholder()) {
-    var width = 0;
     $$('label.fallback').each(function(elt) {
       elt.setStyle({ display: 'inline-block' });
-      width = Math.max(width, elt.getWidth());
-    });
-    $$('label.fallback').each(function(elt) {
-      elt.setStyle({ width: width+'px' });
     });
   }
+  
+  // Fix label widths
+  var width = 0;
+  $$('label:not(.widelabel)').each(function(elt) {
+    if (elt.getStyle('display') != 'none') {
+      width = Math.max(width, elt.getWidth());
+    }
+  });
+  $$('label:not(.widelabel)').each(function(elt) {
+    elt.setStyle({ width: width+'px' });
+  });
 });
